1ff0e674172656697306f58c94b8a5350f788e78
Your Name <you@example.com>
2020-08-23 21:06:22 +0700

    Update Resolution

--- input.py
+++ input.py
@@ -4,14 +4,11 @@ def inputFile(filename, mode):
         with open(filename, mode) as f:
             # Input size
             size = int(f.readline())
-
-            # Input maze
-            # for _ in range(size * size):
-            #     d = f.readline()
-            #     maze.append(list(map(str, d.split('.'))))
             d = f.read().splitlines()
             for i in range(len(d)):
-                maze.append(list(map(str, d[i].split('.'))))
+                # maze.append(list(map(str, d[i].split('.'))))
+                maze.append(list(map(str, d[i].split('\t'))))
+
 
     except IOError as err:
         print(err)
--- main.py
+++ main.py
@@ -1,9 +1,88 @@
 import input
 import random
-if __name__ == '__main__':
-    maze = input.inputFile("map1.txt", "r")
+import itertools
+def toKB(maze, KB, ih, iw, state):
+    sentence = []
+    for i in [-1, 0, 1]:
+        for j in [-1, 0, 1]:
+            # Not diag of maze
+            if i != j:
+                ii = i + ih
+                jj = j + iw
+                if validCell(ii, jj, len(maze)):
+                    sentence.append(state + str(ii) + str(jj))
+    KB.append(sentence)
+    return KB
+def getAllAdj(maze, ih, iw):
+    res = []
+    for i in [-1, 0, 1]:
+        for j in [-1, 0, 1]:
+            # Not diag of maze
+            if i != j:
+                ii = i + ih
+                jj = j + iw
+                if validCell(ii, jj, len(maze)):
+                    res.append(list((ii, jj)))
+    return res
+
+def validCell(i, j, shape):
+    return 0 <= i and i < shape and 0 <= j and j < shape
+
+def Resolution(KB, alpha):
+    clauses = KB.copy()
+    not_alpha = alpha.replace('~', '')
+    clauses.append(not_alpha)
+    new = []
     while True:
-        x_agent, y_agent = random.randint(0, len(maze) - 1), random.randint(0, len(maze) - 1)
-        if maze[x_agent][y_agent] != 'P' or maze[x_agent][y_agent] != 'W':
+        # Tổ hợp chập 2
+        for pair_clauses in itertools.combinations(clauses, 2):
+            resolvents = Resolve(pair_clauses)
+
+def Resolve(pair_clauses):
+    pass
+
+if __name__ == '__main__':
+    # maze = input.inputFile("map1.txt", "r")
+    maze = input.inputFile("maptab.txt", "r")
+
+    # Count the number of gold
+    # TODO
+
+    # Count the number of wumpus
+    # TODO
+
+    # while True:
+    #     x_agent, y_agent = random.randint(0, len(maze) - 1), random.randint(0, len(maze) - 1)
+    #     if maze[x_agent][y_agent] != 'P' and maze[x_agent][y_agent] != 'W':
+    #         break
+    # print(f"Start position for Agent: {x_agent + 1, y_agent + 1}")
+    # print(f"Room: {maze[x_agent][y_agent]}")
+    i_agent = 0
+    j_agent = 0
+    KB = []
+    score = 0
+    while True:
+        cur_states = maze[i_agent][j_agent]
+        if cur_states == 'P' or cur_states == 'W':
+            print("LOSE")
             break
-    print(f"Start position for Agent: {x_agent + 1, y_agent + 1}, Room: {maze[x_agent][y_agent]}")
+        else:
+            KB.append(['~P' + str(i_agent) + str(j_agent)])
+            KB.append(['~W' + str(i_agent) + str(j_agent)])
+            state = [s for s in cur_states]
+            for i in range(len(state)):
+                if state[i] == 'B':
+                    KB = toKB(maze, KB, i_agent, j_agent, 'P')
+                elif state[i] == 'S':
+                    KB = toKB(maze, KB, i_agent, j_agent, 'W')
+                elif state[i] == 'G':
+                    score += 100
+            allAdj = getAllAdj(maze, i_agent, j_agent)
+            for i in range(len(allAdj)):
+                # KB ^ ~alpha
+                check = Resolution(KB, '~P' + str(allAdj[i][0]) + str(allAdj[i][1]))
+                
+
+
+
+
